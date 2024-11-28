4a2f66b040fc1ebf0988e29202249fbfb542dfdc
Aditya Gavandi <adityagavandi1998@gmail.com>
2024-11-28 18:51:37 +0530

    email box update

--- send_email.py
+++ send_email.py
@@ -36,18 +36,21 @@ def send_email(subject, recipient_email, name, position, company,resume_path,pla
     message.set_content(
         f"""
         Dear {name},
-        
-        I hope this email finds you well. I am writing to apply for the 
-        {position} role at {company}, as advertised on {platform}.
 
-        I attached my resume and portfolio attached for your review. I would welcome the opportunity 
-        to discuss how I can contribute to your team.
+        I am excited about the opportunity to apply for the {position} position currently open at {company}. Having a solid foundation with a proven history of successfully executing meaningful projects using Python programming, I look forward to bringing my capabilities and commitment to your team.
 
-        Thank you for considering my application. I look forward to hearing from you.
+        Throughout my professional experience, I have taken assignments that show my technical ability and can solve problems. The high points include:
+        Social Media Website: A fully functional social media website was designed and developed using Python, Django, and SQLite. This project won the second prize in a design competition for social networking sites, hence speaking to its robustness and user-friendly attributes.
+        Job Application Automation Tool: I developed an application based on Python that uses libraries such as pandas and requests to automate email communication pertaining to job applications. This project shows my ability to address real-world problems through innovative solutions.
+        Portfolio Website: Designed a personal portfolio that communicated all projects and skills while having responsive design and efficient functionality on the backend side.
 
-        Best regards,
+        In addition to my technical expertise, I can bring a collaborative mindset and the ability to adapt quickly to new challenges and I feel that my skills are well in line with your goals. I have attached my resume and the link to my portfolio at ({portfolio_url}). It would be great if you would want to discuss my experience and expertise and how I might assist in making {company} a success.
+
+        I appreciate your consideration of my application. I look forward to the opportunity to contribute to your team and develop professionally with {company}.
+        Warm regards,
         Aditya Anand Gavandi
-        Python Django Developer
+        +91 9637980861
+        adityagavandi1998@gmail.com
         """,
         subtype="plain",
     )
@@ -56,27 +59,48 @@ def send_email(subject, recipient_email, name, position, company,resume_path,pla
     message.add_alternative(
         f"""
         <html>
-        <body style="color: black; font-family: Arial, Helvetica, sans-serif; font-size: 14px;">
-            <p>Dear {name},</p>
-            <p>
-                I hope this email finds you well. I am writing to apply for the 
-                <strong style="font-weight: bold;">{position}</strong> role at <strong style="font-weight: bold;">{company}</strong>, as advertised on 
-                <em style="font-style: italic;">{platform}</em>.
-            </p>
-            <p>
-                I attached my resume and portfolio for your review. I would welcome the opportunity 
-                to discuss how I can contribute to your team.
-            </p>
-            <p>Thank you for considering my application. I look forward to hearing from you.</p>
-            <p>Best regards,</p>
-            <div>
-                <strong style="font-size: 16px;" >Aditya Anand Gavandi</strong><br>
-                Python Django Developer<br>
-                📧 <a href="mailto:adityagavandi1998@gmail.com">adityagavandi1998@gmail.com</a> | 📞 +91 9637980861<br>
-                <a href={linkedIn_url} target="_blank">LinkedIn</a> | 
-                <a href={github_url} target="_blank">GitHub</a> | 
-                <a href="{portfolio_url}" target="_blank">Portfolio</a> | 
-            </div>
+        <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; margin: 20px;">
+        <p>Dear <strong>{name}</strong>,</p>
+
+        <p>
+            I am excited about the opportunity to apply for the <strong>{position}</strong> position currently open at <strong>{company}</strong>. Having a solid foundation with a proven history of successfully executing meaningful projects using Python programming, I look forward to bringing my capabilities and commitment to your team.
+        </p>
+
+        <p>
+            Throughout my professional experience, I have taken assignments that show my technical ability and problem-solving skills. The high points include:
+        </p>
+
+        <ul>
+            <li>
+                <strong>Social Media Website:</strong> A fully functional social media website designed and developed using Python, Django, and SQLite. This project won second prize in a design competition for social networking sites, highlighting its robustness and user-friendly attributes.
+            </li>
+            <li>
+                <strong>Job Application Automation Tool:</strong> A Python-based application utilizing libraries like <code>pandas</code> and <code>requests</code> to automate email communication for job applications. This project showcases my ability to address real-world problems through innovative solutions.
+            </li>
+            <li>
+                <strong>Portfolio Website:</strong> A personal portfolio designed to showcase all projects and skills, featuring responsive design and efficient backend functionality.
+            </li>
+        </ul>
+
+        <p>
+            In addition to my technical expertise, I bring a collaborative mindset and the ability to adapt quickly to new challenges. I feel that my skills align well with your goals. I have attached my resume and included the link to my portfolio here:
+            <a href="{portfolio_url}" target="_blank">Link</a>. I would be delighted to discuss my experience and expertise further and explore how I might assist in making <strong>{company}</strong> a success.
+        </p>
+
+        <p>
+            I appreciate your consideration of my application. I look forward to the opportunity to contribute to your team and develop professionally with <strong>{company}</strong>.
+        </p>
+
+        <p>
+            Warm regards,<br>
+            <strong>Aditya Anand Gavandi</strong>
+        </p>
+        <p>
+        📧 <a href="mailto:adityagavandi1998@gmail.com">adityagavandi1998@gmail.com</a> | 📞 +91 9637980861<br>
+        <a href={linkedIn_url} target="_blank">LinkedIn</a> |
+        <a href={github_url} target="_blank">GitHub</a> |
+        <a href="{portfolio_url}" target="_blank">Portfolio</a> |
+        </p>
         </body>
         </html>
         """,
